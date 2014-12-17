a1138689fbd29ac9a438d54e68154f7de1016d6c
Alexandr Irza <irzyxa@mail.ru>
2014-12-17 10:18:59 +0200

    Added uninstall path for x32

--- volume2/volume2.install/tools/chocolateyUninstall.ps1
+++ volume2/volume2.install/tools/chocolateyUninstall.ps1
@@ -1,8 +1,16 @@
 $packageName = "volume2.install"
 $installerType = 'EXE'
 $silentArgs = '/SILENT'
-$appDir = 'C:\Program Files (x86)\Volume2\uninstall.exe'
 $validExitCodes = @(0,1)
+
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+
+if ($is64bit) {
+  $appDir = "${Env:ProgramFiles(x86)}\Volume2\uninstall.exe"
+} else {
+  $appDir = "${Env:ProgramFiles}\Volume2\uninstall.exe"
+}
 
 try {
   Uninstall-ChocolateyPackage $packageName "$installerType" "$silentArgs" "$appDir" -validExitCodes $validExitCodes
