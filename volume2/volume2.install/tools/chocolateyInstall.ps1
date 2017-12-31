23d90b779f8afffd4a07d41edae13f584c0eea71
Alexandr Irza <irzyxa@gmail.com>
2017-12-31 14:52:44 +0200

    Updated Volume² to version 1.1.5.404

--- volume2/volume2.install/tools/chocolateyInstall.ps1
+++ volume2/volume2.install/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'volume2.install' 
 $installerType = 'EXE'
-$url = 'http://volumesqr.at.ua/Release/Setup_Volume2_1_1_4_347.zip'
+$url = 'http://volumesqr.at.ua/Release/Setup_Volume2_1_1_5_404.zip'
 $silentArgs = '/silent RunProgramSettings_off'
 $validExitCodes = @(0,1)
-$tepmZip = Join-Path $env:TEMP "Setup_Volume2_1_1_4_347.zip"
-$tempExe = Join-Path $env:TEMP "Setup_Volume2_1_1_4_347.exe"
+$tepmZip = Join-Path $env:TEMP "Setup_Volume2_1_1_5_404.zip"
+$tempExe = Join-Path $env:TEMP "Setup_Volume2_1_1_5_404.exe"
 
 try {
   Get-ChocolateyWebFile "$packageName" $tepmZip "$url"
