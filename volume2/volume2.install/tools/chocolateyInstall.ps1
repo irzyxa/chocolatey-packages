bdca63f28f4e7d37a0ed8e329a76fba0c860d6e1
Alexandr Irza <irzyxa@gmail.com>
2022-03-20 20:27:29 +0200

    Updated Volume² to version 1.1.7.450

--- volume2/volume2.install/tools/chocolateyInstall.ps1
+++ volume2/volume2.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'volume2.install' 
 $installerType = 'EXE'
-$url = 'https://drive.google.com/uc?export=download&id=1lIN2uZfMqiTJCJ2LaIoI5EYOv8tefiKC'
+$url = 'https://drive.google.com/uc?export=download&id=1CyOr4OSi7ag6nUdZvvXUNDejSvEsjos1'
 $silentArgs = '/silent RunProgramSettings_off'
 $validExitCodes = @(0,1)
 $tepmZip = Join-Path $env:TEMP "Setup_Volume2_1_1_7_449.zip"
